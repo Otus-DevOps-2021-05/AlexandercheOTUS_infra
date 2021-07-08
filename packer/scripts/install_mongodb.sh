db74c4fde74499cc1a9c67199baf80041ff51199
Alexander Cherkasov <a.cherkasov@dcoa.ru>
2021-07-08 23:47:38 +0300

    Mod packer and terraform configs

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -2,7 +2,8 @@
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get -y update
-apt-get -y install mongodb-org
+sleep 30
+apt-get -y install mongodb-org lsof bash-completion
 systemctl enable mongod.service
 systemctl start mongod.service
 # systemctl status mongod.service
--- packer/scripts/install_ruby.sh
+++ packer/scripts/install_ruby.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt-get update
 sleep 30
-apt-get -y install ruby-full ruby-bundler build-essential
+apt-get -y install ruby-full ruby-bundler build-essential lsof bash-completion
